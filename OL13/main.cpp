c6cca1fc45fb38bc3f69e6a30f8efb658cb89505
MaximeGarnier <maxime.garnier.etudes@gmail.com>
2017-05-17 18:55:13 +0200

    Supression des setCodecFor.. disparuent avec Qt 5

--- OL13/main.cpp
+++ OL13/main.cpp
@@ -42,8 +42,6 @@ void test1(){
 int test2(int argc,char *argv[])
 {
     // UTF-8 Encoding
-    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
-    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
 
     QApplication app(argc, argv);
